365a21e6c4974d087c74948863afbf5854939538
hugo.nunes <hugo.nunes@escolar.ifrn.edu.br>
2023-07-22 11:52:53 -0300

    att

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Categoria,Pessoa,Produto
 # Register your models here.
+
+@admin.register(Categoria)
+
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome_categoria',)
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display= ('nome_produto', )
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome')
